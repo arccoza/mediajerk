321c5cbaac9a89a0e6de02ff003b9748b1e58c2f
Adrien <adrien@arccoza.com>
2025-09-22 14:33:15 +0200

    feat: add append_to_response support for TMDB API
    - Add response types for videos, images, and credits
    - Implement custom UnmarshalJSON for TVSeriesDetails to extract season/N keys into AppendedSeasons array
    - Add append fields to MovieDetails for terminal endpoints
    - Support dynamic season extraction from append_to_response parameters

--- backend/tmdb/types.go
+++ backend/tmdb/types.go
@@ -1,5 +1,12 @@
 package tmdb
 
+import (
+	"encoding/json"
+	"regexp"
+	"sort"
+	"strconv"
+)
+
 type CommonSearchParams struct {
 	Query        string
 	IncludeAdult bool
@@ -122,6 +129,27 @@ type MovieDetails struct {
 	Video               bool                `json:"video"`
 	VoteAverage         float64             `json:"vote_average"`
 	VoteCount           int                 `json:"vote_count"`
+
+	// Append response fields (populated by custom UnmarshalJSON)
+	Videos  *VideosResponse  `json:"videos,omitempty"`
+	Images  *ImagesResponse  `json:"images,omitempty"`
+	Credits *CreditsResponse `json:"credits,omitempty"`
+}
+
+func (m *MovieDetails) UnmarshalJSON(data []byte) error {
+	// Create alias to avoid recursion
+	type MovieDetailsAlias MovieDetails
+
+	// Unmarshal normally - the append fields are handled by normal JSON tags
+	var alias MovieDetailsAlias
+	if err := json.Unmarshal(data, &alias); err != nil {
+		return err
+	}
+
+	// Copy all fields
+	*m = MovieDetails(alias)
+
+	return nil
 }
 
 type CreatedBy struct {
@@ -199,6 +227,82 @@ type TVSeriesDetails struct {
 	Type                string              `json:"type"`
 	VoteAverage         float64             `json:"vote_average"`
 	VoteCount           int                 `json:"vote_count"`
+
+	// Append response fields (populated by custom UnmarshalJSON)
+	FullSeasons []TVSeasonDetails `json:"-"` // From season/N keys
+	Videos      *VideosResponse   `json:"videos,omitempty"`
+	Images      *ImagesResponse   `json:"images,omitempty"`
+	Credits     *CreditsResponse  `json:"credits,omitempty"`
+}
+
+func (t *TVSeriesDetails) UnmarshalJSON(data []byte) error {
+	// Create alias to avoid recursion
+	type TVSeriesDetailsAlias TVSeriesDetails
+
+	// First unmarshal normally into alias
+	var alias TVSeriesDetailsAlias
+	if err := json.Unmarshal(data, &alias); err != nil {
+		return err
+	}
+
+	// Copy all normal fields
+	*t = TVSeriesDetails(alias)
+
+	// Parse raw JSON to extract dynamic season keys
+	var raw map[string]json.RawMessage
+	if err := json.Unmarshal(data, &raw); err != nil {
+		return err
+	}
+
+	// Extract season/N keys
+	seasonRegex := regexp.MustCompile(`^season/(\d+)$`)
+	var seasons []TVSeasonDetails
+	var seasonNumbers []int
+
+	for key, value := range raw {
+		if matches := seasonRegex.FindStringSubmatch(key); matches != nil {
+			seasonNum, err := strconv.Atoi(matches[1])
+			if err != nil {
+				continue
+			}
+
+			var season TVSeasonDetails
+			if err := json.Unmarshal(value, &season); err != nil {
+				continue
+			}
+
+			seasons = append(seasons, season)
+			seasonNumbers = append(seasonNumbers, seasonNum)
+		}
+	}
+
+	// Sort seasons by season number
+	if len(seasons) > 0 {
+		// Create pairs of (seasonNumber, season) for sorting
+		type seasonPair struct {
+			num    int
+			season TVSeasonDetails
+		}
+
+		pairs := make([]seasonPair, len(seasons))
+		for i := range seasons {
+			pairs[i] = seasonPair{seasonNumbers[i], seasons[i]}
+		}
+
+		sort.Slice(pairs, func(i, j int) bool {
+			return pairs[i].num < pairs[j].num
+		})
+
+		// Extract sorted seasons
+		sortedSeasons := make([]TVSeasonDetails, len(pairs))
+		for i, pair := range pairs {
+			sortedSeasons[i] = pair.season
+		}
+
+		t.FullSeasons = sortedSeasons
+	}
+
+	return nil
 }
 
 type TVSeasonDetails struct {
@@ -248,4 +352,72 @@ type EpisodeGroupDetails struct {
 		Order    int       `json:"order"`
 		Episodes []Episode `json:"episodes"`
 	} `json:"groups"`
+}
+
+// Append response types for common TMDB append_to_response endpoints
+type Video struct {
+	ID          string `json:"id"`
+	ISO639_1    string `json:"iso_639_1"`
+	ISO3166_1   string `json:"iso_3166_1"`
+	Key         string `json:"key"`
+	Name        string `json:"name"`
+	Official    bool   `json:"official"`
+	PublishedAt string `json:"published_at"`
+	Site        string `json:"site"`
+	Size        int    `json:"size"`
+	Type        string `json:"type"`
+}
+
+type VideosResponse struct {
+	Results []Video `json:"results"`
+}
+
+type Image struct {
+	AspectRatio float64 `json:"aspect_ratio"`
+	Height      int     `json:"height"`
+	ISO639_1    *string `json:"iso_639_1"`
+	FilePath    string  `json:"file_path"`
+	VoteAverage float64 `json:"vote_average"`
+	VoteCount   int     `json:"vote_count"`
+	Width       int     `json:"width"`
+}
+
+type ImagesResponse struct {
+	Backdrops []Image `json:"backdrops"`
+	Logos     []Image `json:"logos"`
+	Posters   []Image `json:"posters"`
+}
+
+type CastMember struct {
+	Adult              bool    `json:"adult"`
+	Gender             int     `json:"gender"`
+	ID                 int     `json:"id"`
+	KnownForDepartment string  `json:"known_for_department"`
+	Name               string  `json:"name"`
+	OriginalName       string  `json:"original_name"`
+	Popularity         float64 `json:"popularity"`
+	ProfilePath        *string `json:"profile_path"`
+	CastID             int     `json:"cast_id"`
+	Character          string  `json:"character"`
+	CreditID           string  `json:"credit_id"`
+	Order              int     `json:"order"`
+}
+
+type CrewMember struct {
+	Adult              bool    `json:"adult"`
+	Gender             int     `json:"gender"`
+	ID                 int     `json:"id"`
+	KnownForDepartment string  `json:"known_for_department"`
+	Name               string  `json:"name"`
+	OriginalName       string  `json:"original_name"`
+	Popularity         float64 `json:"popularity"`
+	ProfilePath        *string `json:"profile_path"`
+	CreditID           string  `json:"credit_id"`
+	Department         string  `json:"department"`
+	Job                string  `json:"job"`
+}
+
+type CreditsResponse struct {
+	Cast []CastMember `json:"cast"`
+	Crew []CrewMember `json:"crew"`
 }
