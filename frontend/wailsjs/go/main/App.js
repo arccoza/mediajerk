30518632a43d7ab9cd2843064b9b020f94fa9e1b
Adrien <adrien@arccoza.com>
2025-09-23 23:27:22 +0200

    build: update wails js exports

--- frontend/wailsjs/go/main/App.js
+++ frontend/wailsjs/go/main/App.js
@@ -6,6 +6,6 @@ export function Greet(arg1) {
   return window['go']['main']['App']['Greet'](arg1);
 }
 
-export function SelectFiles() {
-  return window['go']['main']['App']['SelectFiles']();
+export function SelectFiles(arg1) {
+  return window['go']['main']['App']['SelectFiles'](arg1);
 }
